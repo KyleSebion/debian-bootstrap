178e8d2192331f9f9c9602a89e227cc65361e415
root <root@grml>
2024-11-07 23:42:26 -0600

    made uki generation work on kernel install and remove

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,14 +20,57 @@ export DEBIAN_FRONTEND=noninteractive
 LANG=C.UTF-8 debconf-set-selections <<< 'locales locales/default_environment_locale select en_US.UTF-8'
 LANG=C.UTF-8 debconf-set-selections <<< 'locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8'
 apt -y install locales
+apt -y install initramfs-tools systemd-boot
 
-sed -i -re '/\slocalhost(\s|$)/s/$/ debian/' /etc/hosts
+# KS-UKI
+apt -y install binutils wget
+install -d /etc/ks-uki
+wget -O /etc/ks-uki/img.bmp https://people.math.sc.edu/Burkardt/data/bmp/blackbuck.bmp
+cat << 'KS-UKI' > /usr/local/sbin/ks-uki
+#!/bin/bash -e
+declare -A path
+path[stub]=/usr/lib/systemd/boot/efi/linuxx64.efi.stub
+path[osrel]=/usr/lib/os-release
+path[uname]=$(mktemp); echo "$1" > "${path[uname]}"
+path[cmdline]=/etc/kernel/cmdline
+path[splash]=/etc/ks-uki/img.bmp
+path[initrd]=/boot/initrd.img-"$1"
+path[linux]=/boot/vmlinuz-"$1"
+path[efiout]=/efi/EFI/Linux/"$2"-"$1".efi
+alignment="$(objdump -p "${path[stub]}" | mawk '$1=="SectionAlignment" { print(("0x"$2)+0) }')"
+getAligned () { echo $(( $1 + $alignment - $1 % $alignment )); }
+declare -A offs
+getOffsetAfter () { getAligned $(( offs[$1] + $( stat -Lc%s "${path[$1]}" ) )); }
+offs[osrel]=$(getAligned $(objdump -h "${path[stub]}" | mawk 'NF==7 {s=("0x"$3)+0;o=("0x"$4)+0} END {print(s+o)}'))
+offs[uname]=$(getOffsetAfter osrel)
+offs[cmdline]=$(getOffsetAfter uname)
+offs[splash]=$(getOffsetAfter cmdline)
+offs[initrd]=$(getOffsetAfter splash)
+offs[linux]=$(getOffsetAfter initrd)
+declare -a args
+for s in "${!offs[@]}"; do args+=(--add-section ".$s=${path[$s]}" --change-section-vma ".$s=$(printf 0x%x "${offs[$s]}")"); done
+objcopy "${args[@]}" "${path[stub]}" "${path[efiout]}"
+rm "${path[uname]}"
+KS-UKI
+cat << 'KS-UKI' | tee /etc/kernel/postinst.d/zzz-ks-uki /etc/initramfs/post-update.d/zzz-ks-uki > /dev/null
+#!/bin/bash -e
+if [ -z "$1" ]; then echo missing version number >&2; exit 1; fi
+et=$(</etc/kernel/entry-token)
+/usr/local/sbin/ks-uki "$1" "$et"
+rm -r "/efi/$et/$1" "/efi/loader/entries/$et-$1.conf"
+rmdir --ignore-fail-on-non-empty "/efi/$et"
+KS-UKI
+cat << 'KS-UKI' > /etc/kernel/postrm.d/zzz-ks-uki
+#!/bin/bash -e
+if [ -z "$1" ]; then echo missing version number >&2; exit 1; fi
+rmIfExists () { [ -f "$1" ] && rm "$1" || exit 0; }
+rmIfExists /efi/EFI/Linux/"$(</etc/kernel/entry-token)"-"$1".efi
+KS-UKI
+chmod 755 /usr/local/sbin/ks-uki /etc/kernel/post{inst,rm}.d/zzz-ks-uki /etc/initramfs/post-update.d/zzz-ks-uki
 
 echo do_symlinks = no > /etc/kernel-img.conf
-apt -y install linux-image-"$(dpkg --print-architecture)"
-
 echo 'root=LABEL=r console=tty0 console=ttyS0' > /etc/kernel/cmdline
-apt -y install systemd-boot
+apt -y install linux-image-"$(dpkg --print-architecture)"
 
 echo '[Match] Name=enp1s0 [Network] Address=10.10.10.3/24 Gateway=10.10.10.1 DNS=1.1.1.1 DNS=8.8.8.8 [DHCPv4] UseDNS=false [DHCPv6] UseDNS=false' | tr ' ' \\n > /etc/systemd/network/10-enp1s0.network
 systemctl enable systemd-networkd
@@ -45,6 +88,7 @@ echo user:live | chpasswd
 echo 'user ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/010_user-nopasswd
 
 apt -y install wireless-regdb # to get rid of: failed to load regulatory.db
+sed -i -re '/\slocalhost(\s|$)/s/$/ debian/' /etc/hosts
 CEOF
 
 # kludge to get systemd-boot entry in efi (adds needed binds and hides that bootctl is running in a container by hiding files src/basic/virt.c detect_container looks for)
@@ -55,55 +99,6 @@ for m in "${b[@]}"; do mount -B "$d" "$m"; done
 bootctl install
 for m in "${b[@]}"; do umount "$m"; done
 rmdir "$d"
-CEOF
-
-# UKI
-systemd-nspawn -PD "$CHROOT_DIR" /bin/bash -x << 'CEOF'
-set -e
-apt -y install binutils
-install -d /etc/ks-uki
-wget -O /etc/ks-uki/img.bmp https://people.math.sc.edu/Burkardt/data/bmp/blackbuck.bmp
-cat << 'KS-UKI' > /usr/local/sbin/ks-uki
-#!/bin/bash
-set -e
-
-declare -A path
-path[stub]=/usr/lib/systemd/boot/efi/linuxx64.efi.stub
-path[osrel]=/usr/lib/os-release
-path[uname]=$(mktemp); echo "$1" > "${path[uname]}"
-path[cmdline]=/etc/kernel/cmdline
-path[splash]=/etc/ks-uki/img.bmp
-path[initrd]=/boot/initrd.img-"$1"
-path[linux]=/boot/vmlinuz-"$1"
-path[efiout]=/efi/EFI/Linux/debian-"$1".efi
-
-alignment="$(objdump -p "${path[stub]}" | mawk '$1=="SectionAlignment" { print(("0x"$2)+0) }')"
-getAligned () { echo $(( $1 + $alignment - $1 % $alignment )); }
-
-declare -A offs
-getOffsetAfter () { getAligned $(( offs[$1] + $( stat -Lc%s "${path[$1]}" ) )); }
-offs[osrel]=$(getAligned $(objdump -h "${path[stub]}" | mawk 'NF==7 {s=("0x"$3)+0;o=("0x"$4)+0} END {print(s+o)}'))
-offs[uname]=$(getOffsetAfter osrel)
-offs[cmdline]=$(getOffsetAfter uname)
-offs[splash]=$(getOffsetAfter cmdline)
-offs[initrd]=$(getOffsetAfter splash)
-offs[linux]=$(getOffsetAfter initrd)
-
-declare -a args
-for s in "${!offs[@]}"; do args+=(--add-section ".$s=${path[$s]}" --change-section-vma ".$s=$(printf 0x%x "${offs[$s]}")"); done
-objcopy "${args[@]}" "${path[stub]}" "${path[efiout]}"
-rm "${path[uname]}"
-KS-UKI
-chmod 755 /usr/local/sbin/ks-uki
-
-entrySrc="$(bootctl list | grep -m1 source | awk '{print($2)}')"
-if [[ "$entrySrc" =~ [^-]+/([^-]+)-(.*)\.conf ]]; then
-  ks-uki "${BASH_REMATCH[2]}"
-  rm -r "/efi/${BASH_REMATCH[1]}/${BASH_REMATCH[2]}" "$entrySrc"
-  rmdir --ignore-fail-on-non-empty "/efi/${BASH_REMATCH[1]}"
-else
-  echo 'Failed to find kernel version to generate UKI from'
-fi
 CEOF
 
 genfstab -L "$CHROOT_DIR" | grep LABEL=[er] > "$CHROOT_DIR"/etc/fstab
