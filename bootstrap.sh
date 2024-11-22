ad00c890389fdd3a75415a2f37efd2ddc5e1332f
root <root@grml>
2024-11-21 23:55:57 -0600

    caching SIGNPKG for now to speed things up

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ export SIGNPKG=/root/ks-systemd-boot-signer_1.0_all.deb
 export LUKS_PASS=password
 
 apt update
-apt -y install parted dosfstools arch-install-scripts mmdebstrap efibootmgr cryptsetup ruby-rubygems; gem install fpm
+apt -y install parted dosfstools arch-install-scripts mmdebstrap efibootmgr cryptsetup
 wipefs -a "$INSTALL_DEV"*
 parted "$INSTALL_DEV" mklabel gpt mkpart e fat32 4MiB 1020MiB mkpart r 1020MiB 3068MiB set 1 esp on
 udevadm settle
@@ -23,12 +23,13 @@ cp "$UKI_IMG" "$CHROOT_DIR$UKI_IMG"
 cp "$SBCTL" "$CHROOT_DIR$SBCTL"
 
 # Create package to auto-sign systemd-boot
-trigf=/usr/lib/systemd/boot/efi/systemd-bootx64.efi
-bashScript () { printf %s\\n '#!/bin/bash' 'trigf='"'$trigf'" "$@"; }
-fpm -n ks-systemd-boot-signer -s empty -t deb --deb-interest "$trigf" -p "$SIGNPKG" \
-  --after-install <(bashScript '[ "$1" = "configure" ] || [ "$1" = "triggered" ] && rm -f "$trigf".signed && [ -f "$trigf" ] && '"'$SBCTL'"' sign -o "$trigf"{.signed,} || true') \
-  --after-remove  <(bashScript 'rm -f "$trigf".signed')
-mv "$SIGNPKG" "$CHROOT_DIR$SIGNPKG"
+##apt -y install ruby-rubygems; gem install fpm
+##trigf=/usr/lib/systemd/boot/efi/systemd-bootx64.efi
+##bashScript () { printf %s\\n '#!/bin/bash' 'trigf='"'$trigf'" "$@"; }
+##fpm -n ks-systemd-boot-signer -s empty -t deb --deb-interest "$trigf" -p "$SIGNPKG" \
+##  --after-install <(bashScript '[ "$1" = "configure" ] || [ "$1" = "triggered" ] && rm -f "$trigf".signed && [ -f "$trigf" ] && '"'$SBCTL'"' sign -o "$trigf"{.signed,} || true') \
+##  --after-remove  <(bashScript 'rm -f "$trigf".signed')
+##mv "$SIGNPKG" "$CHROOT_DIR$SIGNPKG"
 
 arch-chroot "$CHROOT_DIR" /bin/bash -x << 'CEOF'
 mv /etc/apt/apt.conf.d/99mmdebstrap /etc/apt/apt.conf.d/proxy
@@ -40,6 +41,30 @@ LANG=C.UTF-8 apt -y install locales
 # systemd-boot + SecureBoot
 sbctl create-keys
 sbctl enroll-keys -m
+cat << 'SIGNPKG' | base64 -d > "$SIGNPKG" # generated via fpm command above
+ITxhcmNoPgpkZWJpYW4tYmluYXJ5LyAgMCAgICAgICAgICAgMCAgICAgMCAgICAgNjQ0ICAgICA0
+ICAgICAgICAgYAoyLjAKY29udHJvbC50YXIuZ3ovIDAgICAgICAgICAgIDAgICAgIDAgICAgIDY0
+NCAgICAgNjE3ICAgICAgIGAKH4sIAAAAAAAAA+2X32/TMBDH+5y/whS0vpA4vzMihkDiASSQJk3i
+ZeLBSS6p1cSubAe2Mf53nLYqUzRUEErHj/tIVRr77Lv29D2fPTqbHN+SJcnmaRk/N9+DOAt9P0jS
+KLPjaeCnM5JMH9ps1mvDFCHHcPUn4tGuSnTf6Ql9DAlO4/iH+Q/CeJT/LA6jGfEnjGnPf57/OKxD
+P4U6S1kY+UmcsiJ7VkUh+BmcnoY1Ib1WVC+ZAlrJkq60q6+1ga5yCymNq3kjQNFyyUQDrWy85sZ5
+6N+E/DweNYo3DagJC8Ah/ftpNtZ/moao/2PAhQEF2hA66LzlBd3Jmw7yplBzelfvV2ns2TGU+L+C
+R9dSG9VN6eNQ/xdE4/4vixLU/1F4/IgWXNCC6aUznAT12eKXKsHCUR1xazJ/slk99zYNQYUF4i9h
+q38utJnOxyH9R1Ey1r99Qf0fg9/X/6XVfjAnZ2ReSlHzplcwJx/J7S35PrPrMaEaZk5OyL01Y5i4
+vDu+td3FI0vWUj3EqovStAsyLCKu3Ft/2W3z9Ovg26gesAgdxqM2a0bJdkIfh/r/KAvG+k8CPP+P
+wjkrV6yBnNx/sXc+2JshlyIngec773gJQlvjXqyE/CzsrKikyomQApxXqlxyA6WxFSAnrG2d98xe
+L+wHrMlzZXd9WUHBmXjhvLUHjrWAyr3gN9bady7swo2jCmrWt8Y5V1wqbq5zItfDDGudN7KD9Sba
+pTHrnFK4Yt26Ba+UHRXS7RV3G/4JhPMadKn4erujkHbT/TvZWjz0H48gCIIgCIIgCIIgCIIgR+Ab
+Gq717gAoAAAKZGF0YS50YXIuZ3ovICAgIDAgICAgICAgICAgIDAgICAgIDAgICAgIDY0NCAgICAg
+NDAxICAgICAgIGAKH4sIAAAAAAAAA9PTZ6A5MAACc1NTMA0E6DSYbWhibmRgYGhqZmwOFDczNDBj
+UDClvdMYGEqLSxKLFBToYdVgBHr6pcVFNE4DZMS/uaH5aPzTA0DivzgjsSiVZqmA5Pg3NDAwMhyN
+f3oA5PhPyU+mSRogI/5NzUbLf7oA9PjPLtYtriwuSc1N0U3Kzy/RLc5Mz0ulsIIgPf5NDUfLf/oA
+IuM/OSMxLz01Jz9dL72KZDtAEWxmYoIz/o0MDdHi39zEwIhBwYAG/sUAIzz+5bs5GOZLO6QzMfOe
+2ch3SEHC9eHc5TsWh2YKvA1aW/hq2UvtgB/OnswqDqa11Zavp88x/bznudO9D5bHDm3e3CjENz3M
+ZEey97Sme+z+x3LnGT7o8HwZGtnkt4RL8k9MpIHf+21GzyuW/KqUnDWXyz/B5x2P5sfOeA/FfIHY
++51xcYVCpt/Y5cJ+hjYOdCiMglEwCkbBKBgFo2AUjIJRMApGwSgYBaNgFIyCUTAKRsEoGAWjYBSM
+glEwPAAAHsW2LwAoAAAK
+SIGNPKG
 apt -y install "$SIGNPKG"; rm "$SIGNPKG"
 apt -y install systemd-boot
 
